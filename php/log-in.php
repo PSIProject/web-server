96705a435816f4a629051d01fc969f81a6739631
Samantha Hernandez <milysam05@hotmail.com>
2018-05-05 13:40:46 -0500

    Fixes #10

--- php/log-in.php
+++ php/log-in.php
@@ -5,13 +5,17 @@
 	$email = $_POST ['email'];
 	$password = $_POST ['password'];
 
-	$stmt = $db->prepare('SELECT COUNT(*) FROM collaborator WHERE email = ? AND password = SHA2(?, 256)');
+	$stmt = $db->prepare('SELECT id FROM collaborator WHERE email = ? AND password = SHA2(?, 256)');
 	$stmt->bind_param('ss', $email, $password);
 	$stmt->execute();
-	$stmt->bind_result($result)
+	$stmt->bind_result($id)
 
 	if ($stmt->fetch())
+	{
+		session_start();
+		$_SESSION ['user_id'] = $id;
 		echo 'true';
+	}
 	else
 		echo 'wrong data';
 
