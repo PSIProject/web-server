ce4c2a58779ac45e550c1c20b56aeddc92182260
Samantha Hernandez <milysam05@hotmail.com>
2018-05-07 18:37:52 -0500

    Fixes #18

--- php/create-team.php
+++ php/create-team.php
@@ -1,4 +1,19 @@
 <?php
+/////////////////////////////////////////////
+/// Check that the user doesn't have another
+/// team with the same name, if doesn't have
+///	it, then create the team and registered
+///	the user like a collaborator of it.
+///
+/// User POST method, needs the name for the
+///	team an take the id of the user from session
+///	var.
+///
+///	Return true, if the team is created, false
+/// for system errors or 'already exists' if
+///	the user have another team with that name.
+///////////////////////////////////////////////
+
 	require 'connect-db.inc';
 
 	session_start();
@@ -23,7 +38,22 @@
 		if (!$stmt->execute())
 			echo 'false';
 		else
+		{
+			$stmt->close ();
+
+			$stmt= $db->prepare('SELECT LAST_INSERT_ID()');
+	        $stmt->execute();
+			$stmt->bind_result($team_id);
+			$stmt->fetch();
+			$stmt->close();
+
+			$stmt = $db->prepare('INSERT INTO collaborate VALUES (?, ?)');
+			$stmt->bind_param('ii', $manager_id, $team_id);
+			$stmt->execute();
+			$stmt->bind_result($result);
+			$stmt->fetch();
 			echo 'true';
+		}
 	}
     else
         echo 'already exist';
